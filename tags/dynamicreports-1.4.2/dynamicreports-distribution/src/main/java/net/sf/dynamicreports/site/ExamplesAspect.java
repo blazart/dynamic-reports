59d5f6b257c1ef08c5ae974be265095588bd5e84
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2010-09-22 07:30:19 +0000

    [maven-release-plugin]  copy for tag dynamicreports-1.4.2

---
+++ tags/dynamicreports-1.4.2/dynamicreports-distribution/src/main/java/net/sf/dynamicreports/site/ExamplesAspect.java
@@ -0,0 +1,103 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.site;
+
+import net.sf.dynamicreports.examples.Templates;
+import net.sf.dynamicreports.examples.complex.AbstractReportMain;
+import net.sf.dynamicreports.jasper.builder.JasperConcatenatedReportBuilder;
+import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.jasper.builder.export.JasperHtmlExporterBuilder;
+import net.sf.dynamicreports.jasper.builder.export.JasperPdfExporterBuilder;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+@Aspect
+public class ExamplesAspect {	
+	private static String name = "";
+		
+	@Around("show()")
+	public JasperReportBuilder show(ProceedingJoinPoint pjp) throws Throwable {
+		JasperReportBuilder reportBuilder = (JasperReportBuilder) pjp.getTarget();
+		GenerateSite.generateExampleImage(name, reportBuilder);
+		return reportBuilder;
+	}
+	
+	@Pointcut("execution(public net.sf.dynamicreports.jasper.builder.JasperReportBuilder net.sf.dynamicreports.jasper.builder.JasperReportBuilder.show())")
+	public void show() {
+	}
+	
+	@Around("build()")
+	public void build(ProceedingJoinPoint pjp) throws Throwable {
+		Class<? extends Object> design = pjp.getTarget().getClass();
+		name = design.getSimpleName().replaceAll("Main", "");
+		if (pjp.getTarget() instanceof AbstractReportMain<?, ?>) {
+			design = Class.forName(design.getName().replaceAll("Main", "Design"));
+		}
+		String path = design.getName().substring(Templates.class.getPackage().getName().length() + 1).split("\\.")[0];
+		GenerateSite.addExample(name, path, design);
+		pjp.proceed();
+	}
+	
+	@Pointcut("execution(void net.sf.dynamicreports.examples..build())")
+	public void build() {
+	}
+	
+	@Around("toPdf()")
+	public JasperReportBuilder toPdf(ProceedingJoinPoint pjp) throws Throwable {
+		JasperReportBuilder reportBuilder = (JasperReportBuilder) pjp.getTarget();
+		GenerateSite.generateExampleImage(name, reportBuilder, (JasperPdfExporterBuilder) pjp.getArgs()[0]);
+		return reportBuilder;
+	}
+	
+	@Pointcut("execution(public net.sf.dynamicreports.jasper.builder.JasperReportBuilder net.sf.dynamicreports.jasper.builder.JasperReportBuilder.toPdf(*))")
+	public void toPdf() {
+	}
+	
+	@Around("toHtml()")
+	public JasperReportBuilder toHtml(ProceedingJoinPoint pjp) throws Throwable {
+		JasperReportBuilder reportBuilder = (JasperReportBuilder) pjp.getTarget();
+		GenerateSite.generateExampleImage(name, reportBuilder, (JasperHtmlExporterBuilder) pjp.getArgs()[0]);
+		return reportBuilder;
+	}
+	
+	@Pointcut("execution(public net.sf.dynamicreports.jasper.builder.JasperReportBuilder net.sf.dynamicreports.jasper.builder.JasperReportBuilder.toHtml(*))")
+	public void toHtml() {
+	}
+	
+	@Around("toConcatenatedPdf()")
+	public JasperConcatenatedReportBuilder toConcatenatedPdf(ProceedingJoinPoint pjp) throws Throwable {
+		JasperConcatenatedReportBuilder reportBuilder = (JasperConcatenatedReportBuilder) pjp.getTarget();
+		GenerateSite.generateExampleImage(name, reportBuilder, (JasperPdfExporterBuilder) pjp.getArgs()[0]);
+		return reportBuilder;
+	}
+	
+	@Pointcut("execution(public net.sf.dynamicreports.jasper.builder.JasperConcatenatedReportBuilder net.sf.dynamicreports.jasper.builder.JasperConcatenatedReportBuilder.toPdf(*))")
+	public void toConcatenatedPdf() {
+	}
+}
