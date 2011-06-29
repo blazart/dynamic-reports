d24850919b7cf699e7e8d995d0d3581d880f58d5
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2011-06-29 07:47:40 +0000

    [maven-release-plugin]  copy for tag dynamicreports-2.1.1

---
+++ tags/dynamicreports-2.1.1/trunk/dynamicreports-core/src/main/java/net/sf/dynamicreports/report/base/barcode/DRBarcode.java
@@ -0,0 +1,104 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2011 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.report.base.barcode;
+
+import net.sf.dynamicreports.report.base.component.DRDimensionComponent;
+import net.sf.dynamicreports.report.constant.BarcodeOrientation;
+import net.sf.dynamicreports.report.constant.BarcodeTextPosition;
+import net.sf.dynamicreports.report.constant.Constants;
+import net.sf.dynamicreports.report.definition.barcode.DRIBarcode;
+import net.sf.dynamicreports.report.definition.expression.DRIExpression;
+
+import org.apache.commons.lang.Validate;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public abstract class DRBarcode extends DRDimensionComponent implements DRIBarcode {
+	private static final long serialVersionUID = Constants.SERIAL_VERSION_UID;
+
+	private DRIExpression<String> codeExpression;
+	private DRIExpression<String> patternExpression;
+	private Double moduleWidth;
+	private BarcodeOrientation orientation;
+	private BarcodeTextPosition textPosition;
+	private Double quietZone;
+	private Double verticalQuietZone;
+
+	public DRIExpression<String> getCodeExpression() {
+		return codeExpression;
+	}
+
+	public void setCodeExpression(DRIExpression<String> codeExpression) {
+		Validate.notNull(codeExpression, "codeExpression must not be null");
+		this.codeExpression = codeExpression;
+	}
+
+	public DRIExpression<String> getPatternExpression() {
+		return patternExpression;
+	}
+
+	public void setPatternExpression(DRIExpression<String> patternExpression) {
+		this.patternExpression = patternExpression;
+	}
+
+	public Double getModuleWidth() {
+		return moduleWidth;
+	}
+
+	public void setModuleWidth(Double moduleWidth) {
+		this.moduleWidth = moduleWidth;
+	}
+
+	public BarcodeOrientation getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(BarcodeOrientation orientation) {
+		this.orientation = orientation;
+	}
+
+	public BarcodeTextPosition getTextPosition() {
+		return textPosition;
+	}
+
+	public void setTextPosition(BarcodeTextPosition textPosition) {
+		this.textPosition = textPosition;
+	}
+
+	public Double getQuietZone() {
+		return quietZone;
+	}
+
+	public void setQuietZone(Double quietZone) {
+		this.quietZone = quietZone;
+	}
+
+	public Double getVerticalQuietZone() {
+		return verticalQuietZone;
+	}
+
+	public void setVerticalQuietZone(Double verticalQuietZone) {
+		this.verticalQuietZone = verticalQuietZone;
+	}
+}
