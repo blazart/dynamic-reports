42d9d742b62b61b79cc8cbd5f5d53011106b50c4
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2011-11-29 12:25:17 +0000

    [maven-release-plugin]  copy for tag dynamicreports-2.3.2

---
+++ tags/dynamicreports-2.3.2/dynamicreports-core/src/main/java/net/sf/dynamicreports/design/base/style/DRDesignParagraph.java
@@ -0,0 +1,116 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2011 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.design.base.style;
+
+import java.util.List;
+
+import net.sf.dynamicreports.design.definition.style.DRIDesignParagraph;
+import net.sf.dynamicreports.design.definition.style.DRIDesignTabStop;
+import net.sf.dynamicreports.report.constant.LineSpacing;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public class DRDesignParagraph implements DRIDesignParagraph {
+	private LineSpacing lineSpacing;
+	private Float lineSpacingSize;
+	private Integer firstLineIndent;
+	private Integer leftIndent;
+	private Integer rightIndent;
+	private Integer spacingBefore;
+	private Integer spacingAfter;
+	private Integer tabStopWidth;
+	private List<DRIDesignTabStop> tabStops;
+
+	public LineSpacing getLineSpacing() {
+		return lineSpacing;
+	}
+
+	public void setLineSpacing(LineSpacing lineSpacing) {
+		this.lineSpacing = lineSpacing;
+	}
+
+	public Float getLineSpacingSize() {
+		return lineSpacingSize;
+	}
+
+	public void setLineSpacingSize(Float lineSpacingSize) {
+		this.lineSpacingSize = lineSpacingSize;
+	}
+
+	public Integer getFirstLineIndent() {
+		return firstLineIndent;
+	}
+
+	public void setFirstLineIndent(Integer firstLineIndent) {
+		this.firstLineIndent = firstLineIndent;
+	}
+
+	public Integer getLeftIndent() {
+		return leftIndent;
+	}
+
+	public void setLeftIndent(Integer leftIndent) {
+		this.leftIndent = leftIndent;
+	}
+
+	public Integer getRightIndent() {
+		return rightIndent;
+	}
+
+	public void setRightIndent(Integer rightIndent) {
+		this.rightIndent = rightIndent;
+	}
+
+	public Integer getSpacingBefore() {
+		return spacingBefore;
+	}
+
+	public void setSpacingBefore(Integer spacingBefore) {
+		this.spacingBefore = spacingBefore;
+	}
+
+	public Integer getSpacingAfter() {
+		return spacingAfter;
+	}
+
+	public void setSpacingAfter(Integer spacingAfter) {
+		this.spacingAfter = spacingAfter;
+	}
+
+	public Integer getTabStopWidth() {
+		return tabStopWidth;
+	}
+
+	public void setTabStopWidth(Integer tabStopWidth) {
+		this.tabStopWidth = tabStopWidth;
+	}
+
+	public List<DRIDesignTabStop> getTabStops() {
+		return tabStops;
+	}
+
+	public void setTabStops(List<DRIDesignTabStop> tabStops) {
+		this.tabStops = tabStops;
+	}
+}
---
+++ tags/dynamicreports-2.3.2/dynamicreports-core/src/main/java/net/sf/dynamicreports/report/builder/expression/ExpressionBuilders.java
@@ -0,0 +1,192 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2011 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.report.builder.expression;
+
+import java.awt.Image;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+
+import net.sf.dynamicreports.report.builder.group.GroupBuilder;
+import net.sf.dynamicreports.report.definition.expression.DRIExpression;
+import net.sf.dynamicreports.report.definition.expression.DRISimpleExpression;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public class ExpressionBuilders {
+
+	public PrintInFirstPageExpression printInFirstPage() {
+		return Expressions.printInFirstPage();
+	}
+
+	public PrintNotInFirstPageExpression printNotInFirstPage() {
+		return Expressions.printNotInFirstPage();
+	}
+
+	public PrintWhenGroupHasMoreThanOneRowExpression printWhenGroupHasMoreThanOneRow(String groupName) {
+		return Expressions.printWhenGroupHasMoreThanOneRow(groupName);
+	}
+
+	public PrintWhenGroupHasMoreThanOneRowExpression printWhenGroupHasMoreThanOneRow(GroupBuilder<?> group) {
+		return Expressions.printWhenGroupHasMoreThanOneRow(group);
+	}
+
+	public ReportRowNumberExpression reportRowNumber() {
+		return Expressions.reportRowNumber();
+	}
+
+	public PageRowNumberExpression pageRowNumber() {
+		return Expressions.pageRowNumber();
+	}
+
+	public ColumnRowNumberExpression columnRowNumber() {
+		return Expressions.columnRowNumber();
+	}
+
+	public PageNumberExpression pageNumber() {
+		return Expressions.pageNumber();
+	}
+
+	public ColumnNumberExpression columnNumber() {
+		return Expressions.columnNumber();
+	}
+
+	public GroupRowNumberExpression groupRowNumber(String groupName) {
+		return Expressions.groupRowNumber(groupName);
+	}
+
+	public GroupRowNumberExpression groupRowNumber(GroupBuilder<?> group) {
+		return Expressions.groupRowNumber(group);
+	}
+
+	public ValueExpression<Date> date(Date date) {
+		return Expressions.date(date);
+	}
+
+	public ValueExpression<Number> number(Number number) {
+		return Expressions.number(number);
+	}
+
+	public ValueExpression<Image> image(Image image) {
+		return Expressions.image(image);
+	}
+
+	public ValueExpression<InputStream> inputStream(InputStream inputStream) {
+		return Expressions.inputStream(inputStream);
+	}
+
+	public ValueExpression<URL> url(URL url) {
+		return Expressions.url(url);
+	}
+
+	public <T> ValueExpression<T> value(T value) {
+		return Expressions.value(value);
+	}
+
+	public <T> ValueExpression<T> value(T value, Class<? super T> valueClass) {
+		return Expressions.value(value, valueClass);
+	}
+
+	public ValueExpression<String> text(String text) {
+		return Expressions.text(text);
+	}
+
+	public MessageExpression message(String key) {
+		return Expressions.message(key);
+	}
+
+	public MessageExpression message(String key, Object[] arguments) {
+		return Expressions.message(key, arguments);
+	}
+
+	//jasper
+	public JasperExpression<String> jasper(String text) {
+		return Expressions.jasper(text);
+	}
+
+	public <T> JasperExpression<T> jasper(String expression, Class<? super T> valueClass) {
+		return Expressions.jasper(expression, valueClass);
+	}
+
+	//property
+	public PropertyExpression property(String name, DRISimpleExpression<String> valueExpression) {
+		return Expressions.property(name, valueExpression);
+	}
+
+	public PropertyExpression property(String name, String value) {
+		return Expressions.property(name, value);
+	}
+
+	//parameter
+	public ParameterExpression parameter(String name, DRISimpleExpression<?> valueExpression) {
+		return Expressions.parameter(name, valueExpression);
+	}
+
+	public ParameterExpression parameter(String name, Object value) {
+		return Expressions.parameter(name, value);
+	}
+
+	public PrintInOddRowExpression printInOddRow() {
+		return Expressions.printInOddRow();
+	}
+
+	public PrintInEvenRowExpression printInEvenRow() {
+		return Expressions.printInEvenRow();
+	}
+
+	//subdatasource
+	public BeanCollectionSubDatasourceExpression subDatasourceBeanCollection(String fieldName) {
+		return Expressions.subDatasourceBeanCollection(fieldName);
+	}
+
+	public BeanCollectionSubDatasourceExpression subDatasourceBeanCollection(DRIExpression<? extends Collection<?>> expression) {
+		return Expressions.subDatasourceBeanCollection(expression);
+	}
+
+	public BeanArraySubDatasourceExpression subDatasourceBeanArray(String fieldName) {
+		return Expressions.subDatasourceBeanArray(fieldName);
+	}
+
+	public BeanArraySubDatasourceExpression subDatasourceBeanArray(DRIExpression<? extends Object[]> expression) {
+		return Expressions.subDatasourceBeanArray(expression);
+	}
+
+	public MapCollectionSubDatasourceExpression subDatasourceMapCollection(String fieldName) {
+		return Expressions.subDatasourceMapCollection(fieldName);
+	}
+
+	public MapCollectionSubDatasourceExpression subDatasourceMapCollection(DRIExpression<? extends Collection<Map<String, ?>>> expression) {
+		return Expressions.subDatasourceMapCollection(expression);
+	}
+
+	public MapArraySubDatasourceExpression subDatasourceMapArray(String fieldName) {
+		return Expressions.subDatasourceMapArray(fieldName);
+	}
+
+	public MapArraySubDatasourceExpression subDatasourceMapArray(DRIExpression<? extends Object[]> expression) {
+		return Expressions.subDatasourceMapArray(expression);
+	}
+}
