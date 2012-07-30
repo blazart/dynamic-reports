0cfe4b7fc9c7e1957de82aaa1414801a6ff669ee
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2012-07-30 10:22:17 +0000

    [maven-release-plugin]  copy for tag dynamicreports-3.0.0

---
+++ tags/dynamicreports-3.0.0/dynamicreports-core/src/main/java/net/sf/dynamicreports/report/constant/ChartType.java
@@ -0,0 +1,54 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2012 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.report.constant;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public enum ChartType {
+	AREA,
+	STACKEDAREA,
+	BAR,
+	LAYEREDBAR,
+	BAR3D,
+	STACKEDBAR,
+	STACKEDBAR3D,
+	GROUPEDSTACKEDBAR,
+	LINE,
+	PIE,
+	PIE3D,
+	TIMESERIES,
+	DIFFERENCE,
+	XYAREA,
+	XYBAR,
+	XYLINE,
+	SCATTER,
+	MULTI_AXIS,
+	SPIDER,
+	BUBBLE,
+	CANDLESTICK,
+	HIGHLOW,
+	METER,
+	THERMOMETER,
+	GANTT
+}
---
+++ tags/dynamicreports-3.0.0/dynamicreports-core/src/main/java/net/sf/dynamicreports/report/definition/column/DRIBooleanColumn.java
@@ -0,0 +1,58 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2012 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.report.definition.column;
+
+import net.sf.dynamicreports.report.constant.BooleanComponentType;
+import net.sf.dynamicreports.report.constant.ComponentDimensionType;
+import net.sf.dynamicreports.report.definition.DRIValue;
+import net.sf.dynamicreports.report.definition.component.DRIComponent;
+import net.sf.dynamicreports.report.definition.expression.DRIExpression;
+import net.sf.dynamicreports.report.definition.style.DRIReportStyle;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public interface DRIBooleanColumn extends DRIExpression<Boolean>, DRIColumn<DRIComponent>, DRIValue<Boolean> {
+
+	public Integer getWidth();
+
+	public Integer getHeight();
+
+	public ComponentDimensionType getWidthType();
+
+	public ComponentDimensionType getHeightType();
+
+	public DRIExpression<Boolean> getValueExpression();
+
+	public BooleanComponentType getComponentType();
+
+	public Boolean getEmptyWhenNullValue();
+
+	public Integer getImageWidth();
+
+	public Integer getImageHeight();
+
+	public DRIReportStyle getStyle();
+
+	public DRIExpression<Boolean> getPrintWhenExpression();
+}
