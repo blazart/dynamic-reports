5fa86036c6fab6f9e88a2f93de8c05a2391f492c
rmariaca <rmariaca@bf0c503b-2351-4f66-9eae-aa52f3279a3f>
2012-08-28 07:51:53 +0000

    [maven-release-plugin]  copy for tag dynamicreports-3.0.1

---
+++ tags/dynamicreports-3.0.1/dynamicreports-core/src/main/java/net/sf/dynamicreports/jasper/base/export/AbstractJasperExcelExporter.java
@@ -0,0 +1,185 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2012 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.jasper.base.export;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.dynamicreports.jasper.definition.export.JasperIExcelExporter;
+import net.sf.dynamicreports.report.constant.Constants;
+
+/**
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public abstract class AbstractJasperExcelExporter extends AbstractJasperExporter implements JasperIExcelExporter {	
+	private static final long serialVersionUID = Constants.SERIAL_VERSION_UID;
+	
+	private Boolean onePagePerSheet;
+	private Boolean removeEmptySpaceBetweenRows;
+	private Boolean removeEmptySpaceBetweenColumns;
+	private Boolean whitePageBackground;
+	private Boolean detectCellType;
+	private List<String> sheetNames;
+	private Boolean fontSizeFixEnabled;
+	private Boolean imageBorderFixEnabled;
+	private Integer maxRowsPerSheet;
+	private Boolean ignoreGraphics;
+	private Boolean collapseRowSpan;
+	private Boolean ignoreCellBorder;
+	private Boolean ignoreCellBackground;
+	private String password;
+	
+	public AbstractJasperExcelExporter() {
+		this.sheetNames = new ArrayList<String>();
+	}
+	
+	@Override
+	public Boolean getOnePagePerSheet() {
+		return onePagePerSheet;
+	}
+	
+	public void setOnePagePerSheet(Boolean onePagePerSheet) {
+		this.onePagePerSheet = onePagePerSheet;
+	}
+	
+	@Override
+	public Boolean getRemoveEmptySpaceBetweenRows() {
+		return removeEmptySpaceBetweenRows;
+	}
+	
+	public void setRemoveEmptySpaceBetweenRows(Boolean removeEmptySpaceBetweenRows) {
+		this.removeEmptySpaceBetweenRows = removeEmptySpaceBetweenRows;
+	}
+	
+	@Override
+	public Boolean getRemoveEmptySpaceBetweenColumns() {
+		return removeEmptySpaceBetweenColumns;
+	}
+	
+	public void setRemoveEmptySpaceBetweenColumns(Boolean removeEmptySpaceBetweenColumns) {
+		this.removeEmptySpaceBetweenColumns = removeEmptySpaceBetweenColumns;
+	}
+	
+	@Override
+	public Boolean getWhitePageBackground() {
+		return whitePageBackground;
+	}
+	
+	public void setWhitePageBackground(Boolean whitePageBackground) {
+		this.whitePageBackground = whitePageBackground;
+	}
+	
+	@Override
+	public Boolean getDetectCellType() {
+		return detectCellType;
+	}
+	
+	public void setDetectCellType(Boolean detectCellType) {
+		this.detectCellType = detectCellType;
+	}
+	
+	@Override
+	public List<String> getSheetNames() {
+		return sheetNames;
+	}
+	
+	public void setSheetNames(List<String> sheetNames) {
+		this.sheetNames = sheetNames;
+	}
+
+	public void addSheetName(String sheetName) {
+		this.sheetNames.add(sheetName);
+	}
+	
+	@Override
+	public Boolean getFontSizeFixEnabled() {
+		return fontSizeFixEnabled;
+	}
+	
+	public void setFontSizeFixEnabled(Boolean fontSizeFixEnabled) {
+		this.fontSizeFixEnabled = fontSizeFixEnabled;
+	}
+	
+	@Override
+	public Boolean getImageBorderFixEnabled() {
+		return imageBorderFixEnabled;
+	}
+	
+	public void setImageBorderFixEnabled(Boolean imageBorderFixEnabled) {
+		this.imageBorderFixEnabled = imageBorderFixEnabled;
+	}
+	
+	@Override
+	public Integer getMaxRowsPerSheet() {
+		return maxRowsPerSheet;
+	}
+	
+	public void setMaxRowsPerSheet(Integer maxRowsPerSheet) {
+		this.maxRowsPerSheet = maxRowsPerSheet;
+	}
+	
+	@Override
+	public Boolean getIgnoreGraphics() {
+		return ignoreGraphics;
+	}
+	
+	public void setIgnoreGraphics(Boolean ignoreGraphics) {
+		this.ignoreGraphics = ignoreGraphics;
+	}
+	
+	@Override
+	public Boolean getCollapseRowSpan() {
+		return collapseRowSpan;
+	}
+	
+	public void setCollapseRowSpan(Boolean collapseRowSpan) {
+		this.collapseRowSpan = collapseRowSpan;
+	}
+	
+	@Override
+	public Boolean getIgnoreCellBorder() {
+		return ignoreCellBorder;
+	}
+	
+	public void setIgnoreCellBorder(Boolean ignoreCellBorder) {
+		this.ignoreCellBorder = ignoreCellBorder;
+	}
+	
+	@Override
+	public Boolean getIgnoreCellBackground() {
+		return ignoreCellBackground;
+	}
+	
+	public void setIgnoreCellBackground(Boolean ignoreCellBackground) {
+		this.ignoreCellBackground = ignoreCellBackground;
+	}
+	
+	@Override
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
---
+++ tags/dynamicreports-3.0.1/dynamicreports-core/src/main/java/net/sf/dynamicreports/report/builder/column/BooleanColumnBuilder.java
@@ -0,0 +1,205 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2012 Ricardo Mariaca
+ * http://dynamicreports.sourceforge.net
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.dynamicreports.report.builder.column;
+
+import net.sf.dynamicreports.report.base.column.DRBooleanColumn;
+import net.sf.dynamicreports.report.builder.FieldBuilder;
+import net.sf.dynamicreports.report.builder.style.ReportStyleBuilder;
+import net.sf.dynamicreports.report.constant.BooleanComponentType;
+import net.sf.dynamicreports.report.constant.ComponentDimensionType;
+import net.sf.dynamicreports.report.constant.Constants;
+import net.sf.dynamicreports.report.definition.expression.DRIExpression;
+
+import org.apache.commons.lang3.Validate;
+
+/**
+ * It shows a boolean value either as a text or as an image.
+ *
+ * @author Ricardo Mariaca (dynamicreports@gmail.com)
+ */
+public class BooleanColumnBuilder extends ColumnBuilder<BooleanColumnBuilder, DRBooleanColumn> {
+	private static final long serialVersionUID = Constants.SERIAL_VERSION_UID;
+
+	protected BooleanColumnBuilder(FieldBuilder<Boolean> field) {
+		super(new DRBooleanColumn());
+		Validate.notNull(field, "field must not be null");
+		getObject().setValueExpression(field.getField());
+	}
+
+	protected BooleanColumnBuilder(DRIExpression<Boolean> valueExpression) {
+		super(new DRBooleanColumn());
+		getObject().setValueExpression(valueExpression);
+	}
+
+  /**
+   * Sets the preferred width of a column.
+   * @see net.sf.dynamicreports.report.builder.Units
+   *
+   * @param width the column preferred width >= 0
+   * @exception IllegalArgumentException if <code>width</code> is < 0
+   * @return a column builder
+   */
+	public BooleanColumnBuilder setWidth(Integer width) {
+		getObject().setWidth(width);
+		return this;
+	}
+
+  /**
+   * Sets the fixed width of a column.
+   * @see net.sf.dynamicreports.report.builder.Units
+   *
+   * @param width the column fixed width >= 0
+   * @exception IllegalArgumentException if <code>width</code> is < 0
+   * @return a column builder
+   */
+	public BooleanColumnBuilder setFixedWidth(Integer width) {
+		getObject().setWidth(width);
+		getObject().setWidthType(ComponentDimensionType.FIXED);
+		return this;
+	}
+
+  /**
+   * Sets the minimum width of a column.
+   * @see net.sf.dynamicreports.report.builder.Units
+   *
+   * @param width the column minimum width >= 0
+   * @exception IllegalArgumentException if <code>width</code> is < 0
+   * @return a column builder
+   */
+	public BooleanColumnBuilder setMinWidth(Integer width) {
+		getObject().setWidth(width);
+		getObject().setWidthType(ComponentDimensionType.EXPAND);
+		return this;
+	}
+
+  /**
+   * Sets the preferred height of a column.
+   * @see net.sf.dynamicreports.report.builder.Units
+   *
+   * @param height the column preferred height >= 0
+   * @exception IllegalArgumentException if <code>height</code> is < 0
+   * @return a column builder
+   */
+	public BooleanColumnBuilder setHeight(Integer height) {
+		getObject().setHeight(height);
+		return this;
+	}
+
+  /**
+   * Sets the fixed height of a column.
+   * @see net.sf.dynamicreports.report.builder.Units
+   *
+   * @param height the column fixed height >= 0
+   * @exception IllegalArgumentException if <code>height</code> is < 0
+   * @return a column builder
+   */
+	public BooleanColumnBuilder setFixedHeight(Integer height) {
+		getObject().setHeight(height);
+		getObject().setHeightType(ComponentDimensionType.FIXED);
+		return this;
+	}
+
+  /**
+   * Sets the minimum height of a column.
+   * @see net.sf.dynamicreports.report.builder.Units
+   *
+   * @param height the column minimum height >= 0
+   * @exception IllegalArgumentException if <code>height</code> is < 0
+   * @return a column builder
+   */
+	public BooleanColumnBuilder setMinHeight(Integer height) {
+		getObject().setHeight(height);
+		getObject().setHeightType(ComponentDimensionType.EXPAND);
+		return this;
+	}
+
+	/**
+	 * Sets the boolean presentation type.<br/>
+	 * <i>BooleanComponentType.TEXT_*</i> - shows a text value<br/>
+	 * <i>BooleanComponentType.IMAGE_*</i> - shows an image
+	 *
+	 * @param booleanComponentType the component type
+	 * @return a column builder
+	 */
+	public BooleanColumnBuilder setComponentType(BooleanComponentType booleanComponentType) {
+		getObject().setComponentType(booleanComponentType);
+		return this;
+	}
+
+	public BooleanColumnBuilder setEmptyWhenNullValue(Boolean emptyWhenNullValue) {
+		getObject().setEmptyWhenNullValue(emptyWhenNullValue);
+		return this;
+	}
+
+	/**
+	 * Sets the boolean image dimension. Has effect only when the boolean value is presented as an image.
+	 *
+	 * @param width the image width
+	 * @param height the image height
+	 * @return a column builder
+	 */
+	public BooleanColumnBuilder setImageDimension(Integer width, Integer height) {
+		getObject().setImageWidth(width);
+		getObject().setImageHeight(height);
+		return this;
+	}
+
+	/**
+	 * Sets the boolean image width. Has effect only when the boolean value is presented as an image.
+	 *
+	 * @param width the image width
+	 * @return a column builder
+	 */
+	public BooleanColumnBuilder setImageWidth(Integer width) {
+		getObject().setImageWidth(width);
+		return this;
+	}
+
+	/**
+	 * Sets the boolean image height. Has effect only when the boolean value is presented as an image.
+	 *
+	 * @param height the image height
+	 * @return a column builder
+	 */
+	public BooleanColumnBuilder setImageHeight(Integer height) {
+		getObject().setImageHeight(height);
+		return this;
+	}
+
+	@Override
+	public BooleanColumnBuilder setStyle(ReportStyleBuilder style) {
+		if (style != null) {
+			getObject().setStyle(style.getStyle());
+		}
+		else {
+			getObject().setStyle(null);
+		}
+		return this;
+	}
+
+	@Override
+	public BooleanColumnBuilder setPrintWhenExpression(DRIExpression<Boolean> printWhenExpression) {
+		getObject().setPrintWhenExpression(printWhenExpression);
+		return this;
+	}
+}
